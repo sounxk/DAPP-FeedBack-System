5ec7ebca25ba6041f493bf51c89e75b2e68f5c25
c0dewr3ck <51148258+c0dewr3ck@users.noreply.github.com>
2019-12-30 21:04:56 +0530

    Update Feedback.sol

--- contracts/Feedback.sol
+++ contracts/Feedback.sol
@@ -170,14 +170,14 @@ contract MyContract{
         emit votedEvent(_policyAddress);
     }
 
-    function withdraw(uint withdrawAmount) public returns (uint) 
+    function withdraw(uint withdrawAmount) public returns (uint remainingBal) 
 
     {
       /* If the sender's balance is at least the amount they want to withdraw,
          Subtract the amount from the sender's balance, and try to send that amount of ether
          to the user attempting to withdraw. IF the send fails, add the amount back to the user's balance
          return the user's balance.*/
-      address user = msg.sender;
+      address payable user = msg.sender;
 
       // require(withdrawAmount >= owner.balance);
       require(persons[user].credits >= withdrawAmount);
